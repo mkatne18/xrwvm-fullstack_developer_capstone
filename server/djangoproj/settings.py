0a4753e84748ace4987d9b03284cd3f809c6f5bb
mkatne18 <mkatne18@github.com>
2025-03-14 21:19:18 -0400

    changes from strat

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,12 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'meghanakatne-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://meghanakatne-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
-
-]
+ALLOWED_HOSTS=['localhost','https://meghanakatne-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS=['https://meghanakatne-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
 
 
 
@@ -67,10 +63,8 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/static'),
-            os.path.join(BASE_DIR, 'frontend/build'),
-            os.path.join(BASE_DIR, 'frontend/build/static'),
+                        'DIRS': [
+            os.path.join(BASE_DIR,'frontend/static')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -145,7 +139,5 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/static'),
-    os.path.join(BASE_DIR, 'frontend/build'),
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR,'frontend/static')
 ]
